06effbde7d879577acbafb766f96fe34d38e428d
Владимир Сидоров <svs_13@mail.ru>
2019-09-22 14:57:57 +0300

    TAR-1 feature: задан конфиг БД

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=things_archive',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
