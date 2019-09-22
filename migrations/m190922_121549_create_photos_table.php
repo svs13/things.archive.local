2fa52a55f5b0bed2eb5ea93265cc52cb24867451
Владимир Сидоров <svs_13@mail.ru>
2019-09-22 15:20:40 +0300

    TAR-1 feature: создана миграция добавления таблицы photos "Фотографии"

---
+++ migrations/m190922_121549_create_photos_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Добавление таблицы photos "Фотографии"
+ *
+ * Class m190922_121549_create_photos_table
+ */
+class m190922_121549_create_photos_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%photos}}', [
+            'id' => $this->primaryKey(),
+            'entity_type' => $this->string()->notNull(),
+            'entity_id' => $this->integer()->unsigned()->notNull(),
+            'url' => $this->string()->notNull(),
+            'path' => $this->string()->notNull(),
+            'sort' => $this->integer()->unsigned()->notNull(),
+            'created_at' => $this->timestamp()->notNull(),
+        ]);
+
+        $this->createIndex(
+            'idx-photos-entity_type-entity_id',
+            'photos',
+            ['entity_type', 'entity_id']
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropIndex(
+            'idx-photos-entity_type-entity_id',
+            'photos'
+        );
+        $this->dropTable('{{%photos}}');
+    }
+}
